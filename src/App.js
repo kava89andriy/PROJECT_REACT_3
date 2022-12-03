549a20c935f3c41487302c04e5306dd561738e89
Andrii Kavetskyi <kava89andriy@gmail.com>
2022-12-03 08:59:39 +0100

    project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// const convertCurrency = (quantity, code) => {
-//   return fetch(
-//     "https://api.nbp.pl/api/exchangerates/rates/c/" +
-//       code +
-//       "/today/?format=json"
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       let result = data.rates[0].bid * quantity;
-
-//       document.getElementById("result").innerHTML = result.toFixed(2) + " PLN";
-//     });
-// };
-
-// let convertBTN = document.getElementById("convert");
-
-// convertBTN.addEventListener("click", function () {
-//   let currency = document.getElementById("currencies");
-//   let currencyCode = currency.value;
-//   let number = document.getElementById("number");
-//   let quantity = number.value;
-
-//   convertCurrency(quantity, currencyCode);
-// });
-
 import "./App.css";
 import Input from "./Components/Input";
 import Select from "./Components/Select";
@@ -34,11 +9,12 @@ function App() {
   const [amount, setAmount] = useState(0);
   const [result, setResult] = useState(0);
   const onClick = () => {
-    fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`)
+    fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`)
       .then((response) => response.json())
       .then((data) => {
         setResult(`${(data.rates[0].mid * amount).toFixed(2)} PLN`);
-      });
+      })
+      .catch((error) => console.error(error));
   };
   const onInputChange = (value) => {
     setAmount(value);
