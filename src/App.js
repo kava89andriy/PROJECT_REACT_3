717f391e7fcde31956dffc2952855ed7e9698672
Andrii Kavetskyi <kava89andriy@gmail.com>
2022-12-03 06:20:35 +0100

    project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-const convertCurrency = (quantity, code) => {
-  return fetch(
-    "https://api.nbp.pl/api/exchangerates/rates/c/" +
-      code +
-      "/today/?format=json"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let result = data.rates[0].bid * quantity;
+// const convertCurrency = (quantity, code) => {
+//   return fetch(
+//     "https://api.nbp.pl/api/exchangerates/rates/c/" +
+//       code +
+//       "/today/?format=json"
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let result = data.rates[0].bid * quantity;
 
-      document.getElementById("result").innerHTML = result.toFixed(2) + " PLN";
-    });
-};
+//       document.getElementById("result").innerHTML = result.toFixed(2) + " PLN";
+//     });
+// };
 
-let convertBTN = document.getElementById("convert");
+// let convertBTN = document.getElementById("convert");
 
-convertBTN.addEventListener("click", function () {
-  let currency = document.getElementById("currencies");
-  let currencyCode = currency.value;
-  let number = document.getElementById("number");
-  let quantity = number.value;
+// convertBTN.addEventListener("click", function () {
+//   let currency = document.getElementById("currencies");
+//   let currencyCode = currency.value;
+//   let number = document.getElementById("number");
+//   let quantity = number.value;
 
-  convertCurrency(quantity, currencyCode);
-});
+//   convertCurrency(quantity, currencyCode);
+// });
 
 import "./App.css";
 import Input from "./Components/Input";
